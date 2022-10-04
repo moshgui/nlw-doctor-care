3c34b1186be43d860c28ebc1a88b760c9062814f
moshgui <guilherme14_pvh@hotmail.com>
2022-10-03 20:53:40 -0400

    scroll reveal add

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 const buttonToggle = document.getElementById('menu-toggle');
 
+//function que mostra o menu ao clicar no botão
+//versao mobile
 function toggleMenu() {
     const nav = document.getElementById('nav-menu');
     nav.classList.toggle('active');
 }
 
 buttonToggle.addEventListener('click', toggleMenu);
+
+//script biblioteca scrollReveal
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.container', {duration: 2000});
